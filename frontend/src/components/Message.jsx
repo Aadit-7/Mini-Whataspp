519019432395fccb41904b32dc34d764894703ab
Aadit-7 <aadityadav121@gmail.com>
2024-11-03 10:58:05 +0530

    Comments added

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,27 +1,48 @@
-import React, { useEffect, useRef } from 'react'
-import {useSelector} from "react-redux";
+import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 
-const Message = ({message}) => {
-    const scroll = useRef();
-    const {authUser,selectedUser} = useSelector(store=>store.user);
+const Message = ({ message }) => {
+  const scroll = useRef();
 
-    useEffect(()=>{
-        scroll.current?.scrollIntoView({behavior:"smooth"});
-    },[message]);
-    
-    return (
-        <div ref={scroll} className={`chat ${message?.senderId === authUser?._id ? 'chat-end' : 'chat-start'}`}>
-            <div className="chat-image avatar">
-                <div className="w-10 rounded-full">
-                    <img alt="Tailwind CSS chat bubble component" src={message?.senderId === authUser?._id ? authUser?.profilePhoto  : selectedUser?.profilePhoto } />
-                </div>
-            </div>
-            <div className="chat-header">
-                <time className="text-xs opacity-50 text-white">12:45</time>
-            </div>
-            <div className={`chat-bubble ${message?.senderId !== authUser?._id ? 'bg-gray-200 text-black' : ''} `}>{message?.message}</div>
+  // Retrieve authUser and selectedUser from the Redux store
+  const { authUser, selectedUser } = useSelector((store) => store.user);
+
+  // Effect to scroll to the latest message whenever it updates
+  useEffect(() => {
+    scroll.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]); // Dependency on message to trigger scrolling
+
+  return (
+    <div
+      ref={scroll} // Attach the scroll reference to this div
+      className={`chat ${
+        message?.senderId === authUser?._id ? "chat-end" : "chat-start"
+      }`} // Conditional class for message alignment
+    >
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="User avatar"
+            src={
+              message?.senderId === authUser?._id
+                ? authUser?.profilePhoto
+                : selectedUser?.profilePhoto
+            }
+          />
         </div>
-    )
-}
+      </div>
+      <div className="chat-header">
+        <time className="text-xs text-white opacity-50">12:45</time>
+      </div>
+      <div
+        className={`chat-bubble ${
+          message?.senderId !== authUser?._id ? "bg-gray-200 text-black" : ""
+        }`}
+      >
+        {message?.message}
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
